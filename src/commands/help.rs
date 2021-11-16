fadef9069d19bdf4538ffb4f8db3775f4d758099
LogWat <Nk8877wpt+Karan@gmail.com>
2021-11-16 23:11:18 +0900

    unknown_command追加

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -3,14 +3,15 @@ use serenity::prelude::*;
 use std::collections::HashSet;
 use serenity::framework::standard::{
     help_commands,
-    macros::help,
+    macros::{help, hook},
     Args, CommandGroup, HelpOptions,
 };
 use serenity::model::prelude::{channel::Message, id::UserId};
 
 #[help]
-#[individual_command_tip = "Help Command"]
+#[individual_command_tip = "Help Info"]
 #[strikethrough_commands_tip_in_guild = ""]
+// #[lacking_permissions = "Hide"]
 async fn my_help(
     ctx: &Context,
     msg: &Message,
@@ -20,5 +21,13 @@ async fn my_help(
     owners: HashSet<UserId>,
 ) -> CommandResult {
     let _ = help_commands::with_embeds(ctx, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
+#[hook]
+async fn unknown_command(ctx: &Context, msg: &Message, _: &str) -> CommandResult {
+    msg.channel_id
+        .say(&ctx.http, "Unknown command. Try `/help`.")
+        .await?;
     Ok(())
 }
