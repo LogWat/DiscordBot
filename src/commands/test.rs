b4cdf0bfa45664e18e306edcd45490108a47aa83
LogWat <Nk8877wpt+Karan@gmail.com>
2021-11-16 23:49:52 +0900

    add say command

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -2,12 +2,14 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::{
     framework::standard::{
-        macros::{command}, 
+        macros::{command},
+        Args, 
         CommandResult
     },
     model::{
         channel::Message,
     },
+    utils::{content_safe, ContentSafeOptions},
 };
 
 #[command]
@@ -28,6 +30,28 @@ async fn fuckog(ctx: &Context, msg: &Message) -> CommandResult {
     }
     msg.channel_id
         .say(&ctx.http, format!("F*ck you, {}", msg.author.mention()))
+        .await?;
+    Ok(())
+}
+
+#[command]
+async fn say(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    if msg.author.bot {
+        return Ok(());
+    }
+    let settings = if let Some(guild_id) = msg.guild_id {
+        ContentSafeOptions::default()
+            .clean_channel(false)
+            .display_as_member_from(guild_id)
+    } else {
+        ContentSafeOptions::default()
+            .clean_channel(false)
+            .clean_role(false)
+    };
+
+    let content = content_safe(&ctx.cache, &args.rest(), &settings).await;
+    msg.channel_id
+        .say(&ctx.http, content)
         .await?;
     Ok(())
 }
