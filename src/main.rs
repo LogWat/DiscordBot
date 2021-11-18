f2bcafe315e8f17d70bb66002ba702086244474f
LogWat <Nk8877wpt+Karan@gmail.com>
2021-11-18 20:57:32 +0900

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod commands;
 
-use std::{fs::File, io::BufReader, usize, sync::Arc};
+use std::{fs::File, io::BufReader, usize, sync::Arc, collections::{HashSet}};
 
 use serenity::{
     async_trait,
@@ -11,6 +11,7 @@ use serenity::{
         },
         StandardFramework,
     },
+    http::Http,
     model::prelude::{gateway::Ready},
     prelude::*,
 };
@@ -32,7 +33,7 @@ impl TypeMapKey for ShardManagerContainer {
 #[group]
 #[description("General commands")]
 #[summary("General")]
-#[commands(test, say, am_i_admin, shutdown)]
+#[commands(test, say, admin, shutdown)]
 struct General;
 
 struct Handler;
@@ -59,10 +60,32 @@ fn get_token(file_name: &str) -> Result<String> {
 async fn main() {
     // Set the token
     let token = get_token("config.json").expect("[?] Token Not Found");
+
+    let http = Http::new_with_token(&token);
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access user info: {:?}", why),
+            }
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
     // Set Commands
     let framework = StandardFramework::new()
         .configure(|c| c
             .prefix("/")                       // Command Prefix
+            .with_whitespace(true)             // Allow whitespace in prefix
+            .delimiters(vec![",", ", "])       // Delimiters for args
+            .owners(owners)                    // Owners
+            .on_mention(Some(bot_id))          // Allow mentioning the bot
         )
         //.unrecognised_command(unknown_command) // Add Unrecognised Command
         .help(&MY_HELP)                        // Add Help Command
