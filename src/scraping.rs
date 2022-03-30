b505fea58deb4f1987477eb59a959d1b233e9807
LogWat <Nk8877wpt+Karan@gmail.com>
2022-03-30 23:32:03 +0900

    天気表示の改修

--- src/scraping.rs
+++ src/scraping.rs
@@ -30,16 +30,45 @@ pub async fn scraping_weather(ctx: Arc<Context>) -> Result<(), Box<dyn std::erro
     let channel_id: ChannelId = env::var("WEATHER_CHANNEL_ID").expect("Error: WEATHER_CHANNEL_ID is not set.").parse()?;
     let weather_news = env::var("WT_URL").unwrap();
     let mut msg = String::new();
+    let hours = ["03", "06", "09", "12", "15", "18", "21", "24"];
+    for hour in hours.iter() {
+        msg.push_str(&format!("{:^5}", hour));
+    }
+    msg.push_str("\n");
     {
         let doc = scraping_url(&weather_news).await?;
         let selector = Selector::parse(r#"table[id="forecast-point-3h-today"] tbody tr.weather td p"#).unwrap();
-        for node in doc.select(&selector) {
+        for (i, node) in doc.select(&selector).enumerate() {
             let c = node.text().collect::<Vec<_>>();
-            msg.push_str(&format!("{} ", c.join(" ")));
+            if c.len() < 1 {
+                continue;
+            }
+            let w = c[0];
+            match w {
+                "晴れ" => {
+                    if i >= 6 {
+                        msg.push_str(":crescent_moon"); // 夜は月
+                    } else {
+                        msg.push_str(":sunny");
+                    }
+                },
+                "曇り" => msg.push_str(":cloud:"),
+                "雨" => msg.push_str(":umbrella:"),
+                "小雨" => msg.push_str(":closed_umbrella:"),
+                "弱雨" => msg.push_str(":umbrella2:"),
+                "雪" => msg.push_str(":snowflake:"),
+                _ => msg.push_str(":question:"),        // TODO: 大雨とかの追加
+            }
+            msg.push_str(" ");
         }
     }
-    msg.push_str("\n");
-    channel_id.send_message(&ctx, |m| m.content(msg)).await?;
+    channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("今日の天気予報");
+            e.description(msg);
+            e
+        })
+    }).await?;
 
     Ok(())
 }
